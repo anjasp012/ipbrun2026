4c690689933ebf806aaced63f7a064092662d61b
anjas putra <anjasputra012@gmail.com>
2026-04-08 21:59:31 +0700

    'oke'

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 
-#[Fillable(['name', 'qty', 'price', 'display', 'category_id', 'period_id', 'type'])]
+#[Fillable(['name', 'qty', 'price', 'discount', 'display', 'category_id', 'period_id', 'type'])]
 class Ticket extends Model
 {
     use HasUuids;
@@ -15,6 +15,7 @@ class Ticket extends Model
         'name',
         'qty',
         'price',
+        'discount',
         'display',
         'category_id',
         'period_id',
