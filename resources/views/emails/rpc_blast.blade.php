2c33a1763c144b1854324f95e825ed2ee07aed9c
unknown <anjasputra012@gmail.com>
2026-05-30 15:51:29 +0700

    'oke'

--- resources/views/emails/rpc_blast.blade.php
+++ resources/views/emails/rpc_blast.blade.php
@@ -23,8 +23,8 @@
             <tr>
                 <td style="padding:40px 40px 20px;">
                     <p style="margin:0;font-size:16px;line-height:1.6;color:#556677;">
-                        Hai Runner</strong>! 👋<br><br>
-                        Waktunya semakin dekat menuju IPB RUN 2026 – Lestari Untuk Bumi<br><br>
+                        Hai Runner</strong>! 👋<br>
+                        Waktunya semakin dekat menuju IPB RUN 2026 – Lestari Untuk Bumi.<br>
                         Sebelum berlari, pastikan kamu sudah mengambil Racepack milikmu ya.
                     </p>
                 </td>
